c022918ad819cb3ca5c57cc0fe8e87b40a101634
Krishnav-jit-saikia <135402798+Krishnav-jit-saikia@users.noreply.github.com>
2024-02-28 06:21:16 -0800

    Add files via upload

---
+++ SQL/6 .  Order by & Limit & Offsite.sql
@@ -0,0 +1,38 @@
+create table  employee (
+EmpID int primary key , 
+Name varchar(50 ) ,
+Age int ,
+salary int  ,
+address varchar (70) , 
+email varchar (50) 
+) ;
+
+Insert into employee (EmpID , Name , Age , salary , address , email )
+values(1 , 'Krish' , 18 , '50000000 ' , 'Assam ' , 'krishsaik14@gmail.com' ) ,
+(2 , 'John' , 20 , '4500000 ' , 'Delhi ' , 'Johnad176@gmail.com' ) ,
+(3 , 'Devid' , 25 , '2800000 ' , 'Mumbai ' , 'MrDevid34@gmail.com' ) ,
+(4 , 'Sem' , 28 , '5000000 ' , 'Chennai ' , 'SemSuzer344@gmail.com' ) ,
+(5 , 'Bruce' , 17 , '3500000 ' , 'Kolkata ' , 'Bruceandarson45@gmail.com' ) ,
+(6 , 'Adwin' , 22 , '25000000 ' , 'Pune ' , 'AdwinMethew234@gmail.com' ) ,
+(7 , 'Staffy' , 21 , '900000 ' , 'Gujrat ' , 'MissStaffy14@gmail.com' ) ;
+
+select * from employee ;
+
+# Order by -- Used for sorting
+
+select salary from employee order by salary ;-- for accending order
+select salary from employee order by salary desc ;  -- for dcending order
+
+# Limit -- Helps to retrive specific number of records ( extracting top records )
+
+select * from employee limit 3 ;
+
+# offset -- to skip the rows from top 
+
+select * from employee limit 5 offset 1;
+
+select * from employee order by salary desc
+ limit 5 offset 1;
+ 
+
+ 
